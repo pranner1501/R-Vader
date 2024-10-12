be2267ffd72995d48694fd3e733802a86672b959
pranjalsoni <128229165+pranner1501@users.noreply.github.com>
2024-10-13 03:33:35 +0530

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,12 @@
+<?php  
+$a = 45; 
+ $b = 78;  
+echo "Before swapping:<br>";  
+echo "a =".$a."  b=".$b; 
+ // Swapping Logic 
+ $c = $a;  
+$a = $b; 
+ $b = $c;  
+echo "<br><br>After swapping:<br>";  
+echo "a =".$a."  b=".$b;  
+?>  
